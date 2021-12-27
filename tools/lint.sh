e7230f6613a0d87bc90db6745d08e06ff76e731d
Edoardo Arnaudo <edoardo.arn@gmail.com>
2021-12-27 17:10:11 +0000

    Update CI

--- tools/lint.sh
+++ tools/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 # format with black and isort, then run flake8
-black clidantic tests --check
+poetry run black clidantic tests --check
 isort clidantic tests --check-only
 flake8 --max-line-length=120 clidantic tests
