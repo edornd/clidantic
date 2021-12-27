9414949a438a070d38ecc386ad3450fb1828ffc1
edoardo.arnaudo@linksfoundation.com <edoardo.arnaudo@linksfoundation.com>
2021-12-27 16:57:38 +0000

    Add tests to linting

--- tools/lint.sh
+++ tools/lint.sh
@@ -5,4 +5,4 @@ set -x
 # format with black and isort, then run flake8
 black clidantic tests --check
 isort clidantic tests --check-only
-flake8 --max-line-length=120 clidantic
+flake8 --max-line-length=120 clidantic tests
