0389d8b2d884087788a2ef30fdded8df952f3c18
edoardo.arnaudo@linksfoundation.com <edoardo.arnaudo@linksfoundation.com>
2021-12-27 16:57:14 +0000

    Switch to full-poetry versioning

--- clidantic/__init__.py
+++ clidantic/__init__.py
@@ -1,4 +1,3 @@
 from clidantic.core import Parser
-from clidantic.version import __version__
 
-__all__ = ["Parser", "__version__"]
+__all__ = ["Parser"]
--- tools/lint.sh
+++ tools/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 # format with black and isort, then run flake8
-black typer tests --check
-isort typer tests --check-only
+black clidantic tests --check
+isort clidantic tests --check-only
 flake8 --max-line-length=120 clidantic
