e934010dd604775b33972b224831c831b1057512
Edoardo Arnaudo <edoardo.arn@gmail.com>
2022-01-07 16:57:01 +0000

    Add coverage report

--- tools/test.sh
+++ tools/test.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 # Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-poetry run pytest --cov=clidantic
+poetry run pytest --cov=clidantic --cov=examples --cov-report=xml -o console_output_style=progress
